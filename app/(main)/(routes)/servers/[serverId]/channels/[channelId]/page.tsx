050e60221b53caff3e0eb2523c7fa615011b0b89
Veskata98 <vescko332@gmail.com>
2024-03-03 09:08:23 +0200

    Created chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from '@/lib/currentProfile';
 import { db } from '@/lib/db';
 
 import { ChatHeader } from '@/components/chat/ChatHeader';
+import { ChatInput } from '@/components/chat/ChatInput';
 
 interface ChannelIdPageProps {
     params: {
@@ -40,6 +41,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+            <div className="flex-1">Future Messages</div>
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{ channelId: channel.id, serverId: params.serverId }}
+            />
         </div>
     );
 };
